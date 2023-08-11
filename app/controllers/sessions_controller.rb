b1d159e29e1320c8e846d61ed5836c5834502f02
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2023-08-11 21:50:38 +0900

    ログイン機能実装

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(session_params[:email])
+
+    if user & authenticate(session_params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'ログインしました'
+    else
+      render :new
+    end
+    
+  end
+
+  def destroy
+  end
+end

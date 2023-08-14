6e69b0d8309513eb6669353615d685620666ab9b
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2023-08-14 22:10:21 +0900

    milksテストletで共通化

--- spec/system/milks_spec.rb
+++ spec/system/milks_spec.rb
@@ -2,18 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'ミルク管理機能', type: :system do
     describe '一覧表示機能' do
+        let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com', password: 'password') }
+        let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com', password: 'password') }
+
         before do
-            user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com', password: 'password')
             FactoryBot.create(:milk, kinds: 'milk', amount: '150', time: '5:15', memo: 'テスト', user: user_a)
+            visit login_path
+            fill_in 'メールアドレス', with: login_user.email
+            fill_in 'パスワード', with: login_user.password
+            click_button 'ログインする'
         end
 
-        context 'ユーザーが既にログインしている時' do
-            before do
-                visit login_path
-                fill_in 'メールアドレス', with: 'a@example.com'
-                fill_in 'パスワード', with: 'password'
-                click_button 'ログインする'
-            end
+        context 'ユーザーAがログインしている時' do
+            let(:login_user) { user_a }
 
             it 'ユーザーAが登録したミルク記録が表示される' do
                 expect(page).to have_content 'テスト'
@@ -21,13 +22,7 @@ RSpec.describe 'ミルク管理機能', type: :system do
         end
 
         context 'ユーザーBがログインしている時' do
-            before do
-                user_b= FactoryBot.create(:user, name: 'ユーザーB',email: 'b@example.com', password: 'password')
-                visit login_path
-                fill_in 'メールアドレス', with: 'b@example.com'
-                fill_in 'パスワード', with: 'password'
-                click_button 'ログインする'
-            end
+            let(:login_user) { user_b }
 
             it 'ユーザーAで作成したミルク記録が表示されない' do
                 expect(page).to have_no_content 'テスト' 

c09ae97b7b3bc480bbfcca26b31832fb3cb2c73f
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2023-08-17 10:25:12 +0900

    mealモデル単体テスト作成

---
+++ spec/models/meal_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'mealモデルバリデーション' do
+
+    it '種類、量、時間が揃った場合、有効になる' do
+        meal = FactoryBot.build(:meal)
+        meal.valid?
+        expect(meal).to be_valid
+    end
+
+    it '種類がない場合、無効になる' do
+        meal = FactoryBot.build(:meal, kinds: nil)
+        meal.valid?
+        expect(meal.errors[:kinds]).to include('を入力してください')
+    end
+
+    it '量がない場合、無効になる' do
+        meal = FactoryBot.build(:meal, amount: nil)
+        mead.valid?
+        expect(meal.errors[:amount]).to include('を入力してください')
+    end
+
+    it '時間がない場合、無効になる' do
+        meal = FactoryBot.build(:meal, time: nil)
+        meal.valid?
+        expect(meal.errors[:time]).to include('を入力してください')
+    end
+
+end

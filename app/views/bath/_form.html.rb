576f30846abaaac7765bf1b231a0409074f32619
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2023-08-25 06:18:32 +0900

    bathビューファイル編集

---
+++ app/views/bath/_form.html.rb
@@ -0,0 +1,16 @@
+- if bath.errors.present?
+  ul#error_explanation 
+    - bath.errors.full_messages.each do |message|
+      li = message
+
+= form_with model: bath, local: true do |f|
+
+    .form-group 
+      = f.label :time
+      = f.time_field :time, class:'form-control', id: 'bath_time'
+
+    .form-group 
+      = f.label :memo
+      = f.text_area :memo, class:'form-group', id: 'bath_memo'
+      
+    = f.submit '登録する', class: 'btn btn-primary'

1d33fb18d92966f67f38da504804a7868b011aef
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2023-08-28 13:52:27 +0900

    symptomとuserモデル関連づけ

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
     has_many :baths, dependent: :destroy
     has_many :medicines, dependent: :destroy
     has_many :execretions, dependent: :destroy
+    has_many :symptoms, dependent: :destroy
 
     validates :name, presence: true, length: { maximum: 50 }
     validates :email, presence: true, uniqueness: true, length: { maximum: 100 }

77392a1d588a6456462f6322fb8abd9bf8817360
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2023-09-08 07:12:46 +0900

    ホーム画面にheights#newリンクを追加

--- spec/factories/heights.rb
+++ spec/factories/heights.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :height do
-    
+    id { 1 }
+    height { '100.3'}
+    time { '12:21'}
   end
 end
--- spec/models/height_spec.rb
+++ spec/models/height_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Height, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it '正しい情報を入力した時、有効になる' do
+    height = FactoryBot.build(:height)
+    height.valid?
+
+    expect(height).to be_valid
+  end
+
+  it '身長未記入の時、無効になる' do
+    height = FactoryBot.build(:height, height: nil)
+    height.valid?
+
+    expect(height.errors[:height]).to include('を入力してください')
+  end
+
+  it '時間未記入の時、無効になる' do
+    height = FactoryBot.build(:height, time: nil)
+    height.valid?
+
+    expect(height.errors[:time]).to include('を入力してください')
+  end
+
 end

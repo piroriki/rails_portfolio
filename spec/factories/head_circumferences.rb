1acbdd7825a7544c078f4a85fdecc40b04804533
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2023-09-07 13:40:31 +0900

    headcircumferenceモデル単体テスト作成

--- spec/factories/head_circumferences.rb
+++ spec/factories/head_circumferences.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :head_circumference do
+    id { 1 }
+    circumference { '33.5' }
+    time { '15:40' }
+
+    association :user
     
   end
 end
--- spec/models/head_circumference_spec.rb
+++ spec/models/head_circumference_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe HeadCircumference, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it '正しい情報を送信する時、有効になる' do
+    cir = FactoryBot.build(:head_circumference)
+    cir.valid? 
+
+    expect(cir).to be_valid
+  end
+
+  it '頭囲未記入のとき、無効になる' do
+    cir = FactoryBot.build(:head_circumference, circumference: nil)
+    cir.valid?
+
+    expect(cir.errors[:circumference]).to include('を入力してください')
+  end
+
+  it '時間未記入の時、無効になる' do
+    cir = FactoryBot.build(:head_circumference, time: nil)
+    cir.valid?
+
+    expect(cir.errors[:time]).to include('を入力してください')
+  end
+
 end

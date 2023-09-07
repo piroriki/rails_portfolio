12f25ff6d3b1eb5bcae5d3b54ff41969fb45aba0
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2023-09-08 07:03:50 +0900

    heightとuserモデル関連づけ

--- app/models/height.rb
+++ app/models/height.rb
@@ -1,2 +1,7 @@
 class Height < ApplicationRecord
+
+    belongs_to :user
+
+    validates :height, presence: true
+    validates :time, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
     has_many :diaries, dependent: :destroy
     has_many :temperatures, dependent: :destroy
     has_many :head_circumferences, dependent: :destroy
+    has_many :heights, dependent: :destroy
 
     validates :name, presence: true, length: { maximum: 50 }
     validates :email, presence: true, uniqueness: true, length: { maximum: 100 }

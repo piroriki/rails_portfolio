47e1b5885022c7d9f99215c53decaba3048a759a
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2023-09-08 08:56:43 +0900

    weightとuserモデル関連づけ、バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
     has_many :temperatures, dependent: :destroy
     has_many :head_circumferences, dependent: :destroy
     has_many :heights, dependent: :destroy
+    has_many :weights, dependent: :destroy
 
     validates :name, presence: true, length: { maximum: 50 }
     validates :email, presence: true, uniqueness: true, length: { maximum: 100 }
--- app/models/weight.rb
+++ app/models/weight.rb
@@ -1,2 +1,7 @@
 class Weight < ApplicationRecord
+
+    belongs_to :user
+
+    validates :weight, presence: true
+    validates :time, presence: true
 end

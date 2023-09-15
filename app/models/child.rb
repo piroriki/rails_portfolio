c86191223ac62d91bfe2e5b4810eb57d42f01ef4
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2023-09-15 14:28:35 +0900

    childとchild_relation、userモデル関連づけ

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,6 +1,7 @@
 class Child < ApplicationRecord
 
-    belongs_to :user
+    has_many :child_relations, dependent: :destroy, foreign_key: 'child_id'
+    has_many :users, through: :child_relations, source :user
 
     validates :child_name, presence: true
     validates :age, presence: true
--- app/models/child_relation.rb
+++ app/models/child_relation.rb
@@ -1,4 +1,7 @@
 class ChildRelation < ApplicationRecord
   belongs_to :child_id
   belongs_to :user_id
+
+  validates :child_id, presence: true
+  validates :user_id, presence: true
 end

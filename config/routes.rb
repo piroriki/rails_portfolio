326f5a468a1d7291e469d4e02cc102f73c139a54
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2023-09-19 14:22:14 +0900

    childコントローラルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
   root to: 'tops#index' # トップページのみ別コントローラで作成
 
-  resources :milks, :meals, :baths, :medicines, :execretions, :symptoms, :vaccinations, :diaries, :temperatures, :head_circumferences, :heights, :weights
+  resources :milks, :meals, :baths, :medicines, :execretions, :symptoms, :vaccinations, :diaries, :temperatures, :head_circumferences, :heights, :weights, :children
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

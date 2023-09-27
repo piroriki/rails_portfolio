094562c5179af4963dccadd1b03a4a5186525a74
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2023-09-27 10:40:47 +0900

    familyモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_19_210218) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_27_014007) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -102,6 +102,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_19_210218) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_execretions_on_user_id"
+  end
+
+  create_table "families", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "head_circumferences", force: :cascade do |t|

06e1c24d4951e9e7d96f0b7b6031d51c9f72f3a3
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2023-10-11 14:42:39 +0900

    フォロー機能link_to → button_to に変更

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,17 @@
 class RelationshipsController < ApplicationController
     def create
-        follow = current_user.active_relationships.build(follower_id: params[:user_id])
-        follow.save
-        redirect_to users_path, notice: 'フォローしました'
+        @other_user = User.find(params[:follower])
+        current_user.follow(@other_user)
+        #follow = current_user.active_relationships.build(follower_id: params[:user_id])
+        #follow.save
+        #redirect_to users_path, notice: 'フォローしました'
     end
 
     def destroy
-        follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
-        follow.destroy
-        redirect_to root_path, notice: 'フォロー解除しました'
+        @user = current_user.Relationships.find(params[:id]).follower
+        current_user.unfollow(params[:id])
+        #follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
+        #follow.destroy
+        #redirect_to root_path, notice: 'フォロー解除しました'
     end
 end
